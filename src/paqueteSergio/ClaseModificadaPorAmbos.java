2475a396affc2518f710d30d13b32093559f50e3
Usuario <Usuario@DESKTOP-8S08D2L>
2016-04-10 21:28:26 +0200

    Es una clase prueba la cual borraremos del repositorio local y la recuperaremos poco después

---
+++ src/paqueteSergio/ClaseModificadaPorAmbos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package paqueteSergio;
+
+/**
+ *
+ * @author Usuario
+ */
+
+  
+public class ClaseModificadaPorAmbos {      
+     public static void main(String[] args) {  
+           
+System.out.println("Vamos a hacer una prueba sobre esta clase");
+     }         
+}  
