5c641cf947263930ba35b9570df23f683ecb7d41
Usuario <Usuario@DESKTOP-8S08D2L>
2016-04-10 20:59:13 +0200

    Es una clase prueba la cual borraremos del repositorio local y la recuperaremos poco después

---
+++ src/paqueteSergio/ClaseABorrar.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteJavier;
+
+/**
+ *
+ * @author Usuario
+ */
+ 
+  
+public class ClaseABorrar {      
+     public static void main(String[] args) {  
+           
+         System.out.println("Esta clase será borrada del repositorio local y recuperada después");
+             }         
+}  
